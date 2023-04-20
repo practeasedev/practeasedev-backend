c080df31a83e68dc6a4a9ed7021284ce66a47dc5
bhanuteja958 <banu.teja958@gmail.com>
2023-04-20 22:52:30 +0530

    created mongodb atlas server and connected to it

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import './loadenv';
+import mongoose from 'mongoose';
 
-console.log(process.env.PORT);
+//connecting to server
+mongoose
+    .connect(process.env.DB_CONNECTION_URI)
+    .then((res) => {
+        console.info("Successfully connected to mongodb server");
+    })
+    .catch((error) => {
+        console.info("Error while connectiong to mongodb server", error.message);
+    });
