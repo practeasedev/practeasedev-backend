45b7f61271ee659a24727666f625f6e63e9ed5e3
bhanuteja958 <banu.teja958@gmail.com>
2023-08-05 22:21:11 +0530

    changed cookie domain

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -31,7 +31,7 @@ export const SANITIZATION_OPTIONS:IOptions = {
 
 
 export const COOKIE_CONFIG: CookieOptions = {
-  domain: ".vercel.app",
+  domain: ".cyclic.cloud",
   path: '/',
   sameSite: "none",
   secure: true
